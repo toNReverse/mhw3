fbed373de64b5fa6df18de63b469e88701c0933b
toNReverse <mark04sap@gmail.com>
2025-04-07 21:03:56 +0200

    car side page da sistemare

--- mhw2.js
+++ mhw2.js
@@ -5,8 +5,18 @@ document.querySelector('#linksRIGHT a:nth-child(2)').addEventListener('click', (
   });
   
   // Chiude la modale
-  document.querySelector('.close-btn').addEventListener('click', () => {
+  document.querySelector('.close-btn-log').addEventListener('click', () => {
     document.querySelector('#login-modal').classList.remove('show');
     document.querySelector('#login-modal').classList.add('hidden');
   });
   
+// Apre la modale quando clicchi su "Carrello"
+  document.querySelector('#linksRIGHT a:nth-child(3)').addEventListener('click', () => {
+    document.querySelector('#reg-modal').classList.remove('hidden');
+    document.querySelector('#reg-modal').classList.add('show');
+  });
+  // Chiude la modale
+  document.querySelector('.close-btn-reg').addEventListener('click', () => {
+    document.querySelector('#reg-modal').classList.remove('show');
+    document.querySelector('#reg-modal').classList.add('hidden');
+  });  
